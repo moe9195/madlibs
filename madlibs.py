f421bfd90190c831944f3254ed61a82366c84fc3
moe9195 <moerahmeh95@hotmail.com>
2020-02-04 10:57:37 +0200

    Add madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,5 +1,13 @@
 def main():
-	# write your code here
+	print("Mad libs where libs get mad.")
+	time = input("Enter a number from 1 to 12: ")
+	item = input("Enter a noun (plural): ")
+	name = input("Enter a name: ")
+	scream = input("Enter any sentence: ")
+	action = input("Enter a verb: ")
+
+	print("The story goes... \n\nIt was "+time+" o'clock when I heard a knock at the door.\nI opened the door and there was a box full of "+item+" with a note saying \"From Mr. "+name.title()+".\" \nJust as I closed the door I heard a scream \""+scream.upper()+"\".\nI froze in place and all I could do was "+action+".")
+
 
 
 
